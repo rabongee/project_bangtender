dd3b5a42169285d9a217e40fe597c414101a78d5
rabongee <rmarkdehdals@gmail.com>
2024-09-25 19:55:05 +0900

    feat: 회원가입 기능 구현
     - 회원가입을 위한 커스텀 유저 모델 구현
     - 비밀번호 해시화를 위해 외부 라이브러리 사용(bcrypt)
     - 검증 로직을 수행하는 validators 파일 추가
     - 데이터를 직렬화하는 serializers 파일 추가
     - 회원가입 요청시 직렬화된 데이터에 토큰을 담아서 응답함

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,78 @@
+import bcrypt
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
+from liquor.models import Liquor
 
 
-class User(AbstractUser):
-    pass
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, name, email, address):
+        # 외부 라이브러리를 사용하여 비밀번호를 해시화
+        hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
+        user = self.model(
+            username=username,
+            # 바이트 문자열로 데이터베이스에 저장시 문제가 생길수 있어서 유니코드 문자열로 변환
+            password=hashed_password.decode('utf-8'),
+            name=name,
+            # 이메일의 도메인을 대문자로 받아도 소문자로 데이터베이스에 저장
+            email=self.normalize_email(email),
+            address=address
+        )
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, name, password, address=''):
+        user = self.create_user(
+            username=username,
+            password=password,
+            name=name,
+            email=email,
+            address=address
+        )
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=20)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    email = models.EmailField(max_length=254, unique=True)
+    address = models.CharField(max_length=254)
+
+    # 고유 식별자로 사용되는 user모델의 필드 이름
+    USERNAME_FIELD = 'username'
+    # 필수로 입력받고 싶은 값으로 createsuperuser시 사용
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = CustomUserManager()
+
+    def soft_delete(self):
+        self.is_active = False
+        self.save()
+
+
+class BaseLiquorModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class MyLiquor(BaseLiquorModel):
+    liquor = models.ForeignKey(
+        Liquor, on_delete=models.CASCADE, related_name='my_liquor')
+
+
+class LikeLiquor(BaseLiquorModel):
+    liquor = models.ForeignKey(
+        Liquor, on_delete=models.CASCADE, related_name='like_liquor')
+
+
+class DislikeLiquor(BaseLiquorModel):
+    liquor = models.ForeignKey(
+        Liquor, on_delete=models.CASCADE, related_name='dislike_liquor')
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .validators import validator_signup
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class AccountView(APIView):
+    def post(self, request):
+        is_valid, error_message = validator_signup(request.data)
+        if not is_valid:
+            return Response({"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(
+            username=request.data.get("username"),
+            password=request.data.get("password"),
+            name=request.data.get("name"),
+            email=request.data.get("email"),
+            address=request.data.get("address"),
+        )
+        serializer = UserSerializer(user)
+        res_data = serializer.data
+        refresh = RefreshToken.for_user(user)
+        res_data['refresh_token'] = str(refresh)
+        res_data['access_token'] = str(refresh.access_token)
+        return Response(res_data, status=status.HTTP_201_CREATED)
