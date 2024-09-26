db8885900a643eda6119cf13360088d548a99bd9
4FPI <bms1287@gmail.com>
2024-09-26 10:10:16 +0900

    feat cocktail 기능개발
    - 칵테일 모델
    - 칵테일 목록 조회 및 추가
    - 칵테일 상세 조회, 수정
    - 북마크 기능

--- bangtender/settings.py
+++ bangtender/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from config import django_secret_key
 
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framwork',
+    'rest_framework',
     'accounts',
     'liquor',
     'cocktail',
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- cocktail/urls.py
+++ cocktail/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from .views import (
+    CocktailListCreateView, CocktailDetailView, bookmark_cocktail
+)
 
 urlpatterns = [
-    
+    # 칵테일 목록 조회 및 추가 (GET, POST)
+    path('', CocktailListCreateView.as_view(), name='cocktail_list_create'),
+
+    # 특정 칵테일 상세 조회, 수정 및 삭제 (GET, PUT, DELETE)
+    path('<int:pk>/', CocktailDetailView.as_view(), name='cocktail_detail'),
+
+    # 특정 칵테일 북마크 (POST)
+    path('<int:pk>/bookmark/', bookmark_cocktail, name='bookmark_cocktail'),
 ]
