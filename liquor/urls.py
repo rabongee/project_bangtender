aeebcdde90d837d198e0c70fdc698cf0bf20808a
namjimin3123832 <mimi3123832@gmail.com>
2024-09-26 18:58:09 +0900

    1차 postman test 완료
    - liquor 목록, 상세 조회 및 게시글 추가, 수정, 삭제 테스트 통과
    -  urls.py path 수정
    - views.py 코드 수정 및 파라미터 부분 pk로 수정

--- liquor/urls.py
+++ liquor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",  views.LiquorListView.as_view(), name="liquor_list"),
-    path("v1/liquor/<int:pk>/",  views.LiquorDetailView.as_view(), name="liquor_detail"),
-    path("v1/liquor/<int:pk>/bookmark/",  views.LiquorBookmarkView.as_view(), name='liquor_bookmark'),
+    path("", views.LiquorListView.as_view(), name="liquor_list"),
+    path("<int:pk>/", views.LiquorDetailView.as_view(), name="liquor_detail"),
+    path("<int:pk>/bookmark/", views.LiquorBookmarkView.as_view(), name="liquor_bookmark"),
 ]
