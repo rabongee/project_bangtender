1ec9f2e315faefac19ad848a1891c373594d1166
rabongee <rmarkdehdals@gmail.com>
2024-09-27 12:44:23 +0900

    fix: 충돌 해결
    - liquor.urls 에 충돌 현상 해결

--- liquor/urls.py
+++ liquor/urls.py
@@ -2,15 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("",  views.LiquorListView.as_view(), name="liquor_list"),
-    path("<int:pk>/",  views.LiquorDetailView.as_view(), name="liquor_detail"),
-    path("<int:pk>/bookmark/",  views.LiquorBookmarkView.as_view(),
-         name='liquor_bookmark'),
-]
-=======
     path("", views.LiquorListView.as_view(), name="liquor_list"),
     path("<int:pk>/", views.LiquorDetailView.as_view(), name="liquor_detail"),
-    path("<int:pk>/bookmark/", views.LiquorBookmarkView.as_view(), name="liquor_bookmark"),
+    path("<int:pk>/bookmark/", views.LiquorBookmarkView.as_view(),
+         name="liquor_bookmark"),
 ]
->>>>>>> dev
