0bdcb6bf1bb21e7eac4fe94211c006f4aef228b3
namjimin3123832 <mimi3123832@gmail.com>
2024-09-27 11:27:34 +0900

    변경사항 없음

--- liquor/views.py
+++ liquor/views.py
@@ -10,7 +10,8 @@ from .serializers import LiquorListSerializer, LiquorDetailSerializer
 
 # 주류 목록 조회(GET/ 누구나 이용 가능) 및 등록(POST/ 관리자만 가능)
 class LiquorListView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]  # 인증된 회원(회원or관리자)만 가능 or 누구나 이용 가능
+    # 인증된 회원(회원or관리자)만 가능 or 누구나 이용 가능
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     # 주류 목록 조회
     def get(self, request):
@@ -20,10 +21,10 @@ class LiquorListView(APIView):
 
     # 게시글 등록
     def post(self, request):
-        #관리자인지 확인하는 코드 추가
+        # 관리자인지 확인하는 코드 추가
         if not request.user.is_superuser:
             return Response({"detail": "접근 불가 / 관리자만 가능"}, status=status.HTTP_403_FORBIDDEN)
-        
+
         serializer = LiquorDetailSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -49,7 +50,8 @@ class LiquorBookmarkView(APIView):
 
 # 주류 디테일 페이지 조회(GET/누구나 이용 가능) 및 수정(PUT/관리자만), 삭제(DELETE/관리자만)
 class LiquorDetailView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]  # 인증된 회원(회원or관리자)만 가능 or 누구나 이용 가능
+    # 인증된 회원(회원or관리자)만 가능 or 누구나 이용 가능
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     # 주류 디테일 페이지 조회
     def get(self, request, pk):
@@ -60,12 +62,12 @@ class LiquorDetailView(APIView):
     # 게시글 수정
     def put(self, request, pk):
         liquor = get_object_or_404(Liquor, pk=pk)
-        #관리자인지 확인하는 코드 추가
+        # 관리자인지 확인하는 코드 추가
         if not request.user.is_superuser:
             return Response({"detail": "접근 불가 / 관리자만 가능"}, status=status.HTTP_403_FORBIDDEN)
-        
 
-        serializer = LiquorDetailSerializer(liquor, data=request.data, partial=True)  # partial은 부분 수정
+        serializer = LiquorDetailSerializer(
+            liquor, data=request.data, partial=True)  # partial은 부분 수정
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -74,10 +76,11 @@ class LiquorDetailView(APIView):
     # 게시글 삭제
     def delete(self, request, pk):
         liquor = get_object_or_404(Liquor, pk=pk)
-        #관리자인지 확인하는 코드 추가
+        # 관리자인지 확인하는 코드 추가
         if not request.user.is_superuser:
             return Response({"detail": "접근 불가 / 관리자만 가능"}, status=status.HTTP_403_FORBIDDEN)
-        
 
         liquor.delete()
         return Response({"message": "게시글 삭제 완료"}, status=status.HTTP_403_FORBIDDEN)
+
+
